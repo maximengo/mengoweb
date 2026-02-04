1f1f3e5bc7d448306a0b6fcb1347046bff6b362d
Maxi Mengoni <mengo@hotmail.com.ar>
2026-02-03 21:46:03 -0300

    fix mengo foto peso

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import path from "node:path";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),react(),sitemap()],
-  site: 'https://mengo.com.ar',
-   build: {
+  site: "https://mengo.com.ar",
+
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+  ],
+
+  build: {
     inlineStylesheets: "auto",
   },
-});
 
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+  },
+});
